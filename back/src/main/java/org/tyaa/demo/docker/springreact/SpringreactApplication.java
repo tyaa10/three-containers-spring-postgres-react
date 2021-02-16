c2a04cc1e35f7522eafc3c2ee4dfbc687d181d6a
YuriiTrofimenko <tyaa@ukr.net>
2021-02-16 12:23:53 +0200

    init

---
+++ back/src/main/java/org/tyaa/demo/docker/springreact/SpringreactApplication.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.docker.springreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactApplication.class, args);
+	}
+
+}
